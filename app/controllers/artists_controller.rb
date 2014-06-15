bad939a594527e3176e2ff37b4a8f273186dc74a
Aubrey Howell <simplyaubs@gmail.com>
2014-06-15 14:03:53 -0600

    User can update an artist from list.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -15,6 +15,21 @@ class ArtistsController < ApplicationController
     end
   end
 
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update_attributes!(artist_params)
+
+    redirect_to artists_path
+  end
+
     private
     def artist_params
       params.require(:artist).permit(:name, :album)
--- spec/features/artists_spec.rb
+++ spec/features/artists_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD artists' do
     expect(page).to have_content 'Michael Jackson'
     expect(page).to have_content 'Thriller'
   end
+
+  scenario 'User can update an artist' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add an artist'
+    fill_in 'Name', with: 'Michael Jackson'
+    fill_in 'Album', with: 'Thriller'
+    click_on 'Add artist'
+    expect(page).to have_content 'Michael Jackson'
+    expect(page).to have_content 'Thriller'
+    click_on 'Michael Jackson'
+    expect(page).to have_content 'Michael Jackson'
+    expect(page).to have_content 'Thriller'
+    click_on 'Edit'
+    fill_in 'Name', with: 'Van Morrison'
+    fill_in 'Album', with: 'Into the Music'
+    click_on 'Update artist'
+    expect(page).to have_content 'Van Morrison'
+    expect(page).to have_content 'Into the Music'
+    expect(page).to_not have_content 'Michael Jackson'
+    expect(page).to_not have_content 'Thriller'
+  end
 end
